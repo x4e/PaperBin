484f5b6954f7eda5c613b5f47986814344e6dde3
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-04-24 09:19:33 +0100

    Update EntityInsentientTransformer.kt

--- src/main/kotlin/dev/binclub/paperbin/transformers/EntityInsentientTransformer.kt
+++ src/main/kotlin/dev/binclub/paperbin/transformers/EntityInsentientTransformer.kt
@@ -56,10 +56,10 @@ object EntityInsentientTransformer: PaperFeatureTransformer {
 			is EntityEndermite -> false
 			is EntityArmorStand -> false
 			is EntityParrot -> false
-			is EntityPigZombie -> PaperBinInfo.ticks % 25 == 0 // every 60 ticks
-			is EntityVillager -> PaperBinInfo.ticks % 15 == 0 // every 40 ticks
-			is EntityMonster -> PaperBinInfo.ticks % 15 == 0 // every 40 ticks
-			else -> PaperBinInfo.ticks % 5 == 0 // Only every 10 ticks
-		} && entity.world.findNearbyPlayer(entity, 40.0) == null // If there are no players within a 40 block radius then dont calculate AI
+			is EntityPigZombie -> PaperBinInfo.ticks % 25 == 0
+			is EntityVillager -> PaperBinInfo.ticks % 15 == 0
+			is EntityMonster -> PaperBinInfo.ticks % 15 == 0
+			else -> PaperBinInfo.ticks % 5 == 0
+		} && entity.world.findNearbyPlayer(entity, 40.0) != null // If there are no players within a 40 block radius then dont calculate AI
 	}
 }
