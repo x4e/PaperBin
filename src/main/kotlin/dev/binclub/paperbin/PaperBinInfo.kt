346da5fb5f20b167b02f020becda164fd7381986
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-04-28 19:33:38 +0100

    Fix shitty command system

--- src/main/kotlin/dev/binclub/paperbin/PaperBinInfo.kt
+++ src/main/kotlin/dev/binclub/paperbin/PaperBinInfo.kt
@@ -2,28 +2,22 @@ package dev.binclub.paperbin
 
 import dev.binclub.paperbin.transformers.*
 import org.bukkit.Bukkit
-import org.bukkit.Server
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
-import org.bukkit.configuration.file.FileConfiguration
-import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
 import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
-import org.bukkit.generator.ChunkGenerator
-import org.bukkit.plugin.*
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.plugin.java.JavaPluginLoader
-import java.io.File
-import java.io.InputStream
+import org.bukkit.plugin.EventExecutor
+import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.RegisteredListener
+import org.bukkit.plugin.SimplePluginManager
+import java.lang.Compiler.command
 import java.lang.reflect.InvocationHandler
 import java.lang.reflect.Method
 import java.lang.reflect.Proxy
-import java.util.*
-import java.util.logging.Logger
-import kotlin.collections.ArrayList
+
 
 /**
  * @author cookiedragon234 23/Apr/2020
@@ -52,45 +46,27 @@ object PaperBinInfo {
 	fun onStartup() {
 		started = true
 		
-		val manager = Bukkit.getPluginManager() as SimplePluginManager
-		val listeners = SimplePluginManager::class.java.getDeclaredMethod("getEventListeners", Event::class.java::class.java).let {
-			it.isAccessible = true
-			it.invoke(manager, 	PlayerCommandPreprocessEvent::class.java) as HandlerList
-		}
-		
-		val plugin = Proxy.newProxyInstance(
-			this::class.java.classLoader,
-			arrayOf(Plugin::class.java),
-			object: InvocationHandler {
-				override fun invoke(proxy: Any?, method: Method, args: Array<out Any>?): Any {
-					if (method.name == "isEnabled") {
-						return true
-					}
-					throw UnsupportedOperationException("Unsupported method: ${method.name}")
+		Bukkit.getCommandMap().register("binstop", object: Command("binstop") {
+			override fun execute(sender: CommandSender?, commandLabel: String, args: Array<String?>?): Boolean {
+				if (sender?.isOp == true) {
+					enabled = false
+					sender.sendMessage("Stopped paperbin")
+					println("Stopped paperbin")
 				}
+				return true
 			}
-		) as Plugin
+		})
 		
-		listeners.register(RegisteredListener(
-			object: Listener{},
-			EventExecutor { listener, event ->
-				event as PlayerCommandPreprocessEvent
-				
-				if (event.player.isOp) {
-					if (event.message.endsWith("binstop")) {
-						enabled = false
-						event.player.sendMessage("Stopped paperbin")
-					} else if (event.message.endsWith("binstart")) {
-						enabled = true
-						event.player.sendMessage("Started paperbin")
-					}
+		Bukkit.getCommandMap().register("binstart", object: Command("binstart") {
+			override fun execute(sender: CommandSender?, commandLabel: String, args: Array<String?>?): Boolean {
+				if (sender?.isOp == true) {
+					enabled = true
+					sender.sendMessage("Started paperbin")
+					println("Started paperbin")
 				}
-				
-			},
-			EventPriority.NORMAL,
-			plugin,
-			true
-		))
+				return true
+			}
+		})
 		
 		println("Registered event")
 	}
