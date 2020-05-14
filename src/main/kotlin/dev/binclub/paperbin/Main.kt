d687517a0be0c1eccfb645e533e504f625170132
cookiedragon234 <c.o.okiedragon234@gmail.com>
2020-05-14 19:30:54 +0100

    Add support for non paperclip bootstrappers

--- src/main/kotlin/dev/binclub/paperbin/Main.kt
+++ src/main/kotlin/dev/binclub/paperbin/Main.kt
@@ -5,6 +5,9 @@ import java.io.File
 import java.lang.management.ManagementFactory
 import java.net.URL
 import java.net.URLClassLoader
+import java.util.jar.Attributes
+import java.util.jar.Attributes.Name.MAIN_CLASS
+import java.util.jar.JarFile
 
 /**
  * @author cookiedragon234 12/Apr/2020
@@ -33,7 +36,13 @@ fun main(args: Array<String>) {
 		it.invoke(sysCl, file.toURI().toURL())
 	}
 	
-	Class.forName("com.destroystokyo.paperclip.Main")?.getDeclaredMethod("main", Array<String>::class.java)?.also {
-		it.invoke(null, newArgs)
-	} ?: error("Please provide a valid paperclip jar")
+	val mainClass = run {
+		JarFile(file).manifest.mainAttributes.getValue(MAIN_CLASS)
+	}
+	
+	try {
+		Class.forName(mainClass)!!.getDeclaredMethod("main", Array<String>::class.java)!!.invoke(null, newArgs)
+	} catch (t: Throwable) {
+		throw IllegalStateException("Please provide a valid paperclip jar", t)
+	}
 }
