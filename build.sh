52ed822e4e9000268daab9e5c5e9a056667c7376
x4e <x4e_x4e@protonmail.com>
2021-03-02 22:38:44 +0000

    Use cross for CI rust building

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cd ..
 
 ./gradlew build || exit 1
 
-mv nativeloadhook/target/x86_64-apple-darwin/release/libJvmClassHook.dylib build/libs/
-mv nativeloadhook/target/x86_64-pc-windows-gnu/release/JvmClassHook.dll build/libs
 mv nativeloadhook/target/x86_64-unknown-linux-gnu/release/libJvmClassHook.so build/libs
+mv nativeloadhook/target/x86_64-pc-windows-gnu/release/JvmClassHook.dll build/libs
+mv nativeloadhook/target/x86_64-apple-darwin/release/libJvmClassHook.dylib build/libs/
+
