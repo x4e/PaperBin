6c318dc33683863694b412c30b52cee260008764
x4e <x4e_x4e@protonmail.com>
2021-03-02 10:03:18 +0000

    Allow parsing of cargo target

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd nativeloadhook
-cargo build --release --target "x86_64-unknown-linux-gnu" || exit 1
-cargo build --release --target "x86_64-pc-windows-gnu" || exit 1
-cargo build --release --target "x86_64-apple-darwin" || exit 1
+cargo build --release --target x86_64-unknown-linux-gnu || exit 1
+cargo build --release --target x86_64-pc-windows-gnu || exit 1
+cargo build --release --target x86_64-apple-darwin || exit 1
 cd ..
 
 ./gradlew build || exit 1
