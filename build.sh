62d9d5cf4493397658a0b7ba36ef7abc4bfb83b0
x4e <x4e_x4e@protonmail.com>
2021-03-02 23:51:56 +0000

    Bump Version

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm -rf build/libs
+
 cd nativeloadhook
 cargo build --release --target x86_64-unknown-linux-gnu || exit 1
 cargo build --release --target x86_64-pc-windows-gnu || exit 1
